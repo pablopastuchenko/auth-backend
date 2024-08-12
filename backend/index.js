dc2f8c595a09e0c7079688a5a5a58c24b9fc0863
pastuchenko <pablopastuchenkooo@gmail.com>
2024-08-12 13:14:46 -0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './db/connectDB.js';
+
+import authRoutes from './routes/auth.route.js'
+
+dotenv.config()
+const app = express()
+
+app.use("/api/auth", authRoutes)
+
+app.listen(3000, ()=> {
+    connectDB()
+    console.log("Server is running on port 3000");
+    
+})
+
+
+
+// SDZZmgy29xFjlIkD
