3ebb83a455377e6b0b3b25d981bd3a297ecf5cca
pastuchenko <pablopastuchenkooo@gmail.com>
2024-08-19 17:19:39 -0300

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+
 import { connectDB } from './db/connectDB.js';
 
 import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
-app.listen(3000, ()=> {
+app.listen(PORT, ()=> {
     connectDB()
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port: ", PORT);
     
 })
 
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,13 +1,20 @@
 import express from 'express'
-import { login, logout, signup } from '../controllers/auth.controller.js'
+import { login, logout, signup, verifyEmail, forgotPassword, resetPassword, checkAuth  } from '../controllers/auth.controller.js'
+import { verifyToken } from '../middleware/verifyToken.js'
 
 const router = express.Router()
 
-router.get("/signup", signup) 
+router.get("/check-auth", verifyToken, checkAuth)
+router.post("/signup", signup) 
+router.post("/login", login)
+router.post("/logout", logout) 
 
-router.get("/login", login)
+router.post("/verify-email", verifyEmail)
+router.post("/forgot-password", forgotPassword)
 
-router.get("/logout", logout) 
+router.post("/reset-password/:token", resetPassword)
+
+
 
 
 export default router
